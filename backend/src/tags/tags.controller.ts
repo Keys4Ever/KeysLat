90dcd7a4e9b60f4c0f2354727942ab57ab5eb214
Keys4Ever <cubillagonzalo@outlook.com>
2025-03-23 19:30:36 -0300

    ☄️ Feat: Proper responses for tags

--- backend/src/tags/tags.controller.ts
+++ backend/src/tags/tags.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { TagsService } from './tags.service';
 import { CreateTagDto, UpdateTagDto } from 'src/dto/tag.dto';
+import { correctResponse, incorrectResponse } from 'src/utils/buildResponse';
 
 @Controller('tags')
 export class TagsController {
@@ -8,31 +9,55 @@ export class TagsController {
 
     @Post()
     async createTag(@Body() createTagDto: CreateTagDto) {
-        return await this.tagsService.createTag(createTagDto);
+        const result = await this.tagsService.createTag(createTagDto);
+        if(!result){
+            return incorrectResponse('Oops, something happened!');
+        }
+        return correctResponse('Tag created', result);
     }
 
     @Get()
     async findAll(@Query() query: any) {
-        return await this.tagsService.findAll(query.user_id);
+        const result = await this.tagsService.findAll(query.user_id);
+        if(!result){
+            return incorrectResponse('No tags found for user');
+        }
+        return correctResponse('Tags found', result);
     }
 
     @Get(':id')
     async findOne(@Param('id') id: number) {
-        return await this.tagsService.findOne(id);
+        const result = await this.tagsService.findOne(id);
+        if(!result){
+            return incorrectResponse('Tag not found');
+        }
+        return correctResponse('Tag found', result);
     }
 
     @Put(':id')
     async update(@Param('id') id: number, @Body() updateTagDto: UpdateTagDto) {
-        return await this.tagsService.update(id, updateTagDto);
+        const result = await this.tagsService.update(id, updateTagDto);
+        if(!result){
+            return incorrectResponse('Tag not found');
+        }
+        return correctResponse('Tag updated', result);
     }
 
     @Delete(':id')
     async remove(@Param('id') id: number) {
-        return await this.tagsService.delete(id);
+        const result = await this.tagsService.delete(id);
+        if(!result){
+            return incorrectResponse('Tag not found');
+        }
+        return correctResponse('Tag deleted');
     }
 
     @Post(':tagId/urls/:urlId')
     async addTagToUrl(@Param('tagId') tagId: number, @Param('urlId') urlId: number) {
-        return await this.tagsService.addTagToUrl(tagId, urlId);
+        const result = await this.tagsService.addTagToUrl(tagId, urlId);
+        if(!result){
+            return incorrectResponse('Tag not found');
+        }
+        return correctResponse('Tag added to URL', result);
     }
 }
